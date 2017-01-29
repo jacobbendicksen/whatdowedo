5c6fe53d41e75ad5c1f5b72c317a435fa9d1f871
Jacob Bendicksen <jacob.bendicksen@gmail.com>
2017-01-28 23:45:47 -0500

    added issues

--- index.js
+++ index.js
@@ -20,4 +20,34 @@ app.get('/issues.html', function(req, res) {
     console.log("loaded issues.html\n");
 });
 
+app.get('/issues/conflicts.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/issues/conflicts.html'));
+    console.log("loaded conflicts\n");
+});
+
+app.get('/issues/idk.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/issues/idk.html'));
+    console.log("loaded idk\n");
+});
+
+app.get('/issues/lgbt.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/issues/lgbt.html'));
+    console.log("loaded lgbt\n");
+});
+
+app.get('/issues/racism.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/issues/racism.html'));
+    console.log("loaded racism\n");
+});
+
+app.get('/issues/reproductiverights.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/issues/reproductiverights.html'));
+    console.log("loaded reproductiverights\n");
+});
+
+app.get('/issues/refugees.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/issues/refugees.html'));
+    console.log("loaded refugees\n");
+});
+
 app.listen(process.env.PORT || 4000);
